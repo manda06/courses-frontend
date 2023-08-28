8dd240d47968b0a72dd573d8681d0bf0dfe789aa
Kelby Criss <128826649+sdrrfxc@users.noreply.github.com>
2023-08-28 09:44:20 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ dns.setDefaultResultOrder("verbatim");
 export default () => {
   const baseURL =
     process.env.APP_ENV === "development" ? "/" : "/courses-frontend";
-=======
     process.env.APP_ENV === "development" ? "/" : "/courses-frontend/";
 
   return defineConfig({
