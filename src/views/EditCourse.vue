34688b69691b1746b094430c788426afee23ba9d
Ghost-7777 <natecurnutt@yahoo.com>
2023-09-11 15:42:57 -0500

    Edit page test

--- src/views/EditCourse.vue
+++ src/views/EditCourse.vue
@@ -2,7 +2,6 @@
 import { ref, onMounted } from "vue";
 import CourseServices from "../services/courseServices";
 import { useRouter } from "vue-router";
-import courseServices from "../services/courseServices";
 
 const router = useRouter();
 const valid = ref(false);
@@ -19,32 +18,35 @@ const updateCourse = async () => {
     title: Course.value.title,
     description: Course.value.description,
   };
-  try {
-    const response = await CourseServices.update(props.id, data);
-    Course.value.id = response.data.id;
-    router.push({ name: "Courses" });
-  } catch (e) {
-    message.value = e.response.data.message;
-  }
-};
-
-const cancel = () => {
-  router.push({ name: "Courses" });
-};
-
-onMounted(() => {
-  retrieveCourse();
-});
-const retrieveCourse = () => {
-  CourseServices.getAll()
+  await CourseServices.update(props.id, data)
   .then((response) => {
-    courses.value = response.data;
+    response.data.title = Course.value.title,
+    response.data.description = Course.value.description
+    router.push({ name: "courses" });
+
   })
   .catch((e) => {
     message.value = e.response.data.message;
   });
 };
-retrieveCourse();
+
+const cancel = () => {
+  router.push({ name: "courses" });
+};
+
+async function retrieveCourse(){
+  await CourseServices.get(props.id)
+  .then((response) => {
+    Course.value = response.data;
+  })
+  .catch((e) => {
+    message.value = e.response.data.message;
+  });
+};
+
+onMounted(async () => {
+  await retrieveCourse();
+});
 </script>
 
 <template>
